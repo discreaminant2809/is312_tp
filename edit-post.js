1ebf817b63fbd56347904f552652bd69d00f33c9
AyakaMain <phaomaxe2015@gmail.com>
2024-02-20 15:41:57 -0800

    done edit post

---
+++ edit-post.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const doneBtnElem = document.querySelector(`#done-btn`);
+const cancelBtnElem = document.querySelector(`#cancel-btn`);
+
+const toolbarOptions = [
+    [{'font': []}],
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    ['link', 'image'],
+
+    [{'header': 1}, {'header': 2}],
+    [{'list': 'ordered'}, {'list': 'bullet'}],
+    [{'script': 'sub'}, {'script': 'super'}],
+    [{'indent': '+1'}, {'indent': '-1'}],
+    [{'direction': 'rtl'}],
+
+    [{'size': ['small', false, 'large', 'huge']}],
+    [{'header': [1, 2, 3, false]}],
+
+    [{'color': []}, {'background': []}],
+    [{'align': []}],
+
+    ['clean'] // clear-all-formats button
+];
+
+const postContentEditor = new Quill('#post-content', {
+    modules: {
+        toolbar: toolbarOptions
+    },
+    theme: 'snow'
+});
+
+doneBtnElem.onclick = e => {
+    e.preventDefault();
+};
+
+cancelBtnElem.onclick = e => {
+    e.preventDefault();
+};
