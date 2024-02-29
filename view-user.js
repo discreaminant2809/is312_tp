88279c718850a32f71c4d421eb366ce723dd62a8
AyakaMain <phaomaxe2015@gmail.com>
2024-02-29 12:42:30 -0800

    start view user

---
+++ view-user.js
@@ -0,0 +1,10 @@
+const tabElems = document.querySelectorAll(`.tab`);
+
+const SELECTED_CLASS_NAME = `selected`;
+tabElems.forEach((tabElem, _, tabElems) => {
+    tabElem.onclick = () => {
+        tabElems.forEach(tabElem => tabElem.classList.remove(SELECTED_CLASS_NAME));
+        tabElem.classList.add(SELECTED_CLASS_NAME);
+    };
+});
+tabElems[0].classList.add(SELECTED_CLASS_NAME);
