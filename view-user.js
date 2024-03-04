77890748df1ee2ede33e0237f0a5e0578a0f3bb1
AyakaMain <phaomaxe2015@gmail.com>
2024-03-03 20:15:35 -0800

    added tab function for view-user

--- view-user.js
+++ view-user.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const tabElems = document.querySelectorAll(`.tab`);
+const tabContentElems = document.querySelectorAll(`.tab-content`);
 const changePwdTextElem = document.querySelector(`#change-pwd-text`);
 const changePwdForm = document.querySelector(`#change-pwd-form`);
 const changePwdReenterElem = document.querySelector(`#change-pwd-reenter`);
@@ -8,13 +9,16 @@ const changePwdNewPwd = document.querySelector(`#change-pwd-new-pwd`);
 const changePwdCancelElem = document.querySelector(`#change-pwd-cancel`);
 
 const SELECTED_CLASS_NAME = `selected`;
-tabElems.forEach((tabElem, _, tabElems) => {
+tabElems.forEach((tabElem, i, tabElems) => {
     tabElem.onclick = () => {
         tabElems.forEach(tabElem => tabElem.classList.remove(SELECTED_CLASS_NAME));
+        tabContentElems.forEach(tabContentElem => tabContentElem.hidden = true);
         tabElem.classList.add(SELECTED_CLASS_NAME);
+        tabContentElems[i].hidden = false;
     };
 });
 tabElems[0].classList.add(SELECTED_CLASS_NAME);
+tabContentElems[0].hidden = false;
 
 changePwdTextElem.onclick = () => {
     changePwdForm.hidden = false;
